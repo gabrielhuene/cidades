ed377319b4a3f0a4d1172b62365a81df0686925a
gabriel huene <gabriellaurence05@gmail.com>
2022-09-04 16:16:51 -0300

    adiciona a solução do exercício cidades

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Santos");
+Console.WriteLine("São Vicente");
+Console.WriteLine("Praia Grande");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Itabhaém");
+Console.WriteLine("Peruíbe");
